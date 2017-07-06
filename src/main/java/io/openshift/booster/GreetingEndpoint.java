3cbd422f5714b1075fe01c9584b45026fedfa492
LaVern Witherspoon <lwithers@lwithers.rdu.csb>
2017-07-06 13:59:38 -0400

    updated greeting

--- src/main/java/io/openshift/booster/GreetingEndpoint.java
+++ src/main/java/io/openshift/booster/GreetingEndpoint.java
@@ -25,7 +25,7 @@ import javax.ws.rs.QueryParam;
 @Path("/")
 public class GreetingEndpoint {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "Hi Ms. LaVern - Openshift Online wfs-os-online-demo CI-CD pipeline, %s!";
 
     @GET
     @Path("/greeting")
